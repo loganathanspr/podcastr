3c6dc11fd45255ef93a214ac17bf2b7fcc0fa5b5
loganathanspr <loganathanspr@gmail.com>
2024-06-06 09:08:57 +0200

    Add Convex

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://flying-gannet-94.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
